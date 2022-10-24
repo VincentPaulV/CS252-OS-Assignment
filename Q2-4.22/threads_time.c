7978eaa64f5205cbe5357a79dcb4639c58a4a2d0
VincentPaulV <109353180+VincentPaulV@users.noreply.github.com>
2022-10-24 15:33:18 +0530

    threads_time.c added!

---
+++ Q2-4.22/threads_time.c
@@ -0,0 +1,122 @@
+/*  Credits:
+https://gist.github.com/Jabiribn/e58bf13c678953891900e5f982b48037
+*/
+
+#include <stdio.h> 
+#include <sys/time.h>
+#include <stdlib.h>
+#include<pthread.h>
+
+/*  Global Variables    */
+#define MAX_COUNT 5000000
+float array[MAX_COUNT];
+int element_count;
+long long int i;
+
+/*  The Average, Minimum, Maximum Values: */
+float average;
+float minimum;
+float maximum;
+
+/*  Input Function  */
+void input()
+{
+    /*printf("Enter element count: ");
+    scanf("%d",&element_count);*/
+    for(i=0;i<MAX_COUNT;i++)
+    {
+        //scanf("%f",&array[i]);
+        array[i] = i;
+    }
+}
+
+/*  Thread-1    */
+void *thread_average()
+{
+    float sum=0;
+    for(i=0;i<MAX_COUNT;i++)
+    {
+        sum  = sum + array[i];
+    }
+    average = sum / MAX_COUNT ;
+    printf("\nThe average value is %f",average);
+}
+
+/*  Thread-2    */
+void *thread_minimum()
+{
+    float temp;
+    temp = array[0];
+    for(i=0;i<MAX_COUNT;i++)
+    {
+        if(array[i]<temp)
+        {
+            temp = array[i];
+        }
+    }
+    minimum = temp;
+    printf("\nThe minimum value is %f",minimum);
+}
+
+/*  Thread-3    */
+void *thread_maximum()
+{
+    float temp;
+    temp = array[0];
+    for(i=0;i<MAX_COUNT;i++)
+    {
+        if(array[i]>temp)
+        {
+            temp = array[i];
+        }
+    }
+    maximum = temp;
+    printf("\nThe maximum value is %f",maximum);
+}
+
+int main(void)
+{
+    struct timeval TimeValue_Start;
+    struct timezone TimeZone_Start; 
+    struct timeval TimeValue_Final; 
+    struct timezone TimeZone_Final; 
+    long time_start, time_end; 
+    double time_overhead;
+    /*double pi,x;
+    int i,N;
+    pi=0.0;
+    N=1000;*/
+    
+    /*#pragma omp parallel for private(x) reduction(+:pi)
+    for(i=0;i<=N;i++) 
+    {
+        x=(double)i/N;
+        pi+=4/(1+x*x); 
+    }*/
+    input();
+     
+    int i;
+    int threads[3];
+    pthread_t t1;
+    pthread_t t2;
+    pthread_t t3;
+    gettimeofday(&TimeValue_Start, &TimeZone_Start);
+	threads[0]=pthread_create(&t1,NULL,&thread_average,NULL);
+	//pthread_join(t1,NULL);
+	
+    threads[1]=pthread_create(&t2,NULL,&thread_minimum,NULL);
+    //pthread_join(t2,NULL);
+	
+    threads[2]=pthread_create(&t3,NULL,&thread_maximum,NULL);
+    //pthread_join(t3,NULL);
+
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
+
+    gettimeofday(&TimeValue_Final, &TimeZone_Final);
+    time_start = TimeValue_Start.tv_sec * 1000000 + TimeValue_Start.tv_usec; 
+    time_end = TimeValue_Final.tv_sec * 1000000 + TimeValue_Final.tv_usec; 
+    time_overhead = (time_end - time_start)/1000000.0;
+    printf("\n\n\tTime in Seconds (T) : %lf\n",time_overhead);
+}
